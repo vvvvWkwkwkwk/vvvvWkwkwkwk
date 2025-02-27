491958c7636a1dcd1c578c1f9479a64fb386085c
vvvvWkwkwkwk <206932@gardenschool.edu.my>
2025-02-27 23:27:35 +0800

    Create TU06.py

---
+++ TU06.py
@@ -0,0 +1,8 @@
+TwinTowers = 452
+MyCityTallestBuilding = int(input("What's the height of your tallest building in metres?"))
+if TwinTowers > MyCityTallestBuilding:
+    print("The KL Twin Towers is taller than your building! Lol noob get rekt pele triange")
+elif TwinTowers = MyCityTallestBuilding:
+    print("Wait, do we both live in Kuala Lumpur? No way!")
+else:
+    print("Very cool beeg building, unfortunately, KL118.")
