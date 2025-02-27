d916102379f639fceb1aa5643bc3527f436e64a4
vvvvWkwkwkwk <206932@gardenschool.edu.my>
2025-02-27 22:57:22 +0800

    Create TU03.py

---
+++ TU03.py
@@ -0,0 +1,4 @@
+ComputerAge=3.453232
+ComputerName='Skynet'
+MyAge = int(input('What is your age?'))
+print('I am younger than %d, because I am %.3f years old. My name is %s.' % (MyAge,ComputerAge,ComputerName))
